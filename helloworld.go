146a177ed69a252cd4901cefd833ad9e2eb0fa6b
Harsh97x <Rynzie@protonmail.com>
2023-03-16 23:19:20 +0530

    added variables

---
+++ helloworld.go
@@ -0,0 +1,41 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello World")
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("go" + "lang")
+// 	fmt.Println("1+1=", 1+1)
+// 	fmt.Println("7/3=", 7.0/3.0)
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+//   fmt.Println("This" + "is" + "a" + "tutorial")
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var a = "initial"
+// 	fmt.Println(a)
+
+//   var b,c int= 1,2
+//   fmt.Println(b,c)
+
+//   f := "apple"
+//   fmt.Println(f)
+// }
+// this were the variable that are in the go
