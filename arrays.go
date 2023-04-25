804f902c636a52ab29f4f6fec70b9618b75048f1
Harsh97x <Rynzie@protonmail.com>
2023-04-25 11:51:32 +0530

    added fibonacci

--- arrays.go
+++ arrays.go
@@ -14,9 +14,9 @@ func main() {
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
-	var twoD[2][3]int
-	for i:=0; i<2; i++ {
-		for j:=0; j<3; j++ {
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
 			twoD[i][j] = i + j
 		}
 	}
