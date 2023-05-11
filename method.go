fb74af14ae7b85c5b4110f2fc25b4afb07577168
Harsh97x <Rynzie@protonmail.com>
2023-05-12 00:32:25 +0530

    added comments to method.go

--- method.go
+++ method.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-type Human struct {
+type Human struct { // this is a struct which has two fields name and age
 	name string
 	age int
 }
 
-func (h Human)describe() {
-	fmt.Println(h.name, "is", h.age, "years old")
+func (h Human)describe() { // here we take two arguments h & human. h is the instance for human which initializes the field values
+	fmt.Println(h.name, "is", h.age, "years old") // describe method uses human to take objects and print the message
 }
 
 func main() {
-	h:= Human{name: "Jackson", age: 22}
-	h.describe()
+	h:= Human{name: "Jackson", age: 22} // initializes the values
+	h.describe() //here we call describe that prints the message with h's initialize values
 }
