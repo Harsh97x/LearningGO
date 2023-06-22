fe4a85bf45c5f52cd31520a885cb60adbe2b76e0
Harsh97x <Rynzie@protonmail.com>
2023-06-22 13:27:11 +0530

    added variables

---
+++ 02variable/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var username string = "harsh"
+	fmt.Println(username)
+	fmt.Printf("the type of variable is: %T \n", username)
+
+	var isloggedIn bool = true
+	fmt.Println((isloggedIn))
+	fmt.Printf("the type of variable is: %T \n", isloggedIn)
+}
+
+
